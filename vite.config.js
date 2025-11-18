377731bc09a81cda04524f4c3b54df07bd88293e
Ronel <ronelmelendrez01@gmail.com>
2025-11-19 01:04:12 +0800

    Initial commit: Project configuration files

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '/', // Ensure the correct base URL for assets
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue', 'vuetify'], // Optimize chunking by creating a separate chunk for vendor libraries
+        },
+      },
+    },
+  },
+})
