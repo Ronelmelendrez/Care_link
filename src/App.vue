b661b840327626f05c7f6c13ee72977b148a1913
Ronel <ronelmelendrez01@gmail.com>
2025-12-01 00:26:56 +0800

    fix: wrap router-view in Suspense for async setup support

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <v-app>
-    <router-view></router-view>
+    <Suspense>
+      <template #default>
+        <router-view />
+      </template>
+      <template #fallback>
+        <div style="text-align: center; padding: 2rem">
+          <v-progress-circular indeterminate color="primary" size="48" />
+          <div>Loading...</div>
+        </div>
+      </template>
+    </Suspense>
   </v-app>
 </template>
 
